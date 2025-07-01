8ac12cf0f438570eea5bfa1b1f91e531b8b217d3
Marcelo Basso <bassomarcelo.ba@gmail.com>
2025-07-01 11:43:08 -0300

    Update Banner CTA Link

--- data/home.ts
+++ data/home.ts
@@ -3,7 +3,7 @@ export const siteContent = {
 		title: "PRO\nMO\nVE",
 		subtitle: "Sua Plataforma de Eventos em Carlos Barbosa",
 		cta: "Comprar ingressos",
-		ctaLink: "https://www.sympla.com.br/evento/arraia-comedy-carlos-barbosa/2952520",
+		ctaLink: "https://www.sympla.com.br/evento/cine-comedy-garibaldi/3017399",
 	},
 
 	about: {
